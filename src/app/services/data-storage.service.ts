d69e72e7a7500cb17fdc53a40b48faa8b330a32f
ravi singh <raviranjan.rs44@gmail.com>
2020-05-30 14:30:44 +0530

    added images

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -24,12 +24,12 @@ export class DataStorageService {
 
   loginValidation(login:Login):Observable<UserDetails>{
       console.log("in dataservice");
-       return this.http.post<UserDetails>("http://localhost:8080/login",login);
+       return this.http.post<UserDetails>("http://localhost:9090/login",login);
       
   }
   register(register:Register):Observable<UserDetails>{
     console.log("in dataservice");
-      return this.http.post<UserDetails>("http://localhost:8080/register",register);
+      return this.http.post<UserDetails>("http://localhost:9090/register",register);
 }
   //  getUserData():Observable<UserDetails>
   //  {  
@@ -39,10 +39,10 @@ export class DataStorageService {
     //  this.addedToCart= addedToCart;
     this.cartlist.push(list);
     console.log(this.cartlist);
-     return this.http.post<boolean>("http://localhost:8080/addtocart",addToCart);
+     return this.http.post<boolean>("http://localhost:9090/addtocart",addToCart);
    }
    checkout(checkout:Checkout):Observable<boolean>{
-      var flag= this.http.post<boolean>("http://localhost:8080/checkout",checkout);
+      var flag= this.http.post<boolean>("http://localhost:9090/checkout",checkout);
       if(flag){
         this.cartlist=[];
         this.userDetails.cart=[];
